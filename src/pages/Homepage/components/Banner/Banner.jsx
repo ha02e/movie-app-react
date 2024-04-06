c5250bc273ea8da4bc283379345c262ac6eb460a
hayoung <ha02e@naver.com>
2024-04-07 01:02:07 +0900

    에러메세지, 로딩스피너 추가

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import Alert from "react-bootstrap/Alert";
+import Spinner from "react-bootstrap/Spinner";
 
 const Banner = () => {
-  const { data } = usePopularMoviesQuery();
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
   console.log("dddd", data);
-  return <div></div>;
+
+  if (isLoading) {
+    <Spinner animation="border" role="status" variant="warning">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>;
+  }
+  if (isError) {
+    <Alert variant="danger">{error.message}</Alert>;
+  }
+
+  return <div>Banner</div>;
 };
 
 export default Banner;
