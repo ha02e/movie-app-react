eca2b9674e33df39c3d30fe7f7ebf557ff22b09d
hayoung <ha02e@naver.com>
2024-04-07 01:22:57 +0900

    배너 이미지 추가

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
 import Alert from "react-bootstrap/Alert";
 import Spinner from "react-bootstrap/Spinner";
+import "./Banner.style.css";
 
 const Banner = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
@@ -16,7 +17,17 @@ const Banner = () => {
     <Alert variant="danger">{error.message}</Alert>;
   }
 
-  return <div>Banner</div>;
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].backdrop_path}` +
+          ")",
+      }}
+      className="banner-section"
+    ></div>
+  );
 };
 
 export default Banner;
