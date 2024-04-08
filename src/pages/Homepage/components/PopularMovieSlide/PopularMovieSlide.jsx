9ea8548eac4a6fe99187bdcfb9300e6f521acc50
hayoung <ha02e@naver.com>
2024-04-08 16:18:24 +0900

    모바일 사이즈 수정

--- src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -22,12 +22,12 @@ const responsive = {
     // slidesToSlide: 3, // optional, default to 1.
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
+    breakpoint: { max: 1024, min: 753 },
     items: 2,
     // slidesToSlide: 2, // optional, default to 1.
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 753, min: 0 },
     items: 1,
     // slidesToSlide: 1, // optional, default to 1.
   },
